cc57c941fe4545907272746d4ab0e4c352603a6f
Epshtein Dmitry <letuver@yahoo.com>
2021-11-29 02:35:10 +0200

    correcting HW02

--- script.js
+++ script.js
@@ -2,19 +2,18 @@ const num=266219;
 
 let arr=num.toString().split("");
 
-console.log(arr);
 let Sum=arr[0];
-for(let i=0;i<arr.length;i++)
+for(let i=1;i<arr.length;i++)
 {
     Sum*=parseInt(arr[i]);
 }
 console.log(Sum);
 
 let r=Sum**3;
-console.log(r);
+//console.log(r);
 
 let arr2=r.toString().split("");
 console.log(arr2[0],arr2[1]);
 
-const str=r.toString();
-console.log(str.substring(0,2));
+// const str=r.toString();
+// console.log(str.substring(0,2));
