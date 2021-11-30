203b93a44ff576ca831f0663dbb0f247553eb223
Epshtein Dmitry <letuver@yahoo.com>
2021-11-30 02:06:47 +0200

    add test

--- script.js
+++ script.js
@@ -6,6 +6,59 @@ let rollback=20;
 let fullPrice=220;
 let adaptive=true;
 
+title=prompt("Как называется ваш проект?");
+let sPrice;
+do{
+    sPrice=+prompt("Сколько будет стоить данная работа?");
+    if(isNaN(sPrice)){
+        alert("Введите валидное число!");
+    }
+}while(isNaN(sPrice));
+screenPrice=sPrice;
+
+adaptive=confirm("Нужен ли адаптив на сайте?");
+
+
+let service1=prompt("Какой дополнительный тип услуги нужен?");
+do{
+    sPrice=+prompt("Сколько это будет стоить?");
+    if(isNaN(sPrice)){
+        alert("Введите валидное число!");
+    }
+}while(isNaN(sPrice));
+let servicePrice1=sPrice;
+
+
+
+let service2=prompt("Какой дополнительный тип услуги нужен?");
+do{
+    sPrice=+prompt("Сколько это будет стоить?");
+    if(isNaN(sPrice)) {
+        alert("Введите валидное число!");
+    }
+}while(isNaN(sPrice));
+let servicePrice2=sPrice;
+
+
+fullPrice=screenPrice+servicePrice1+servicePrice2;
+
+let servicePercentPrice=fullPrice * (rollback/100);
+
+console.log(Math.ceil(servicePercentPrice));
+
+if(fullPrice>30000) {
+    console.log("Даем скидку в 10%");
+}
+else if(fullPrice>15000) {
+    console.log("Даем скидку в 5%");
+}
+else if(fullPrice>15000) {
+    console.log("Скидка не предусмотрена");
+}
+else {
+    console.log("Что то пошло не так");
+}
+
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
