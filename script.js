7f80beec492358e72a320e2337b6a19ae73f1bf5
Epshtein Dmitry <letuver@yahoo.com>
2021-12-01 17:23:22 +0200

    Check variables

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 "use strict";
 let title = prompt("Как называется ваш проект?","калькулятор");
-if(typeof title != String){
+if(title == null) {
+    title = "no title";
+}
+else{
+    if(typeof title != String){
     title = title.toString();
 }
+}
+
 
 let screens = prompt("Какие типы экранов нужно разработать?","Простые");
-if(typeof screens != String){
+if(screens == null) {
+    screens = "no screen";
+}
+else{
+    if(typeof screens != String){
     screens = screens.toString();
+    }
 }
 
 let rollback = 20;
@@ -25,9 +36,7 @@ do {
 }
 }while(isNaN(sPrice)||(sPrice<0));
 let screenPrice=sPrice;
-//console.log(screenPrice);
 
-//let screenPrice =+ prompt("Сколько будет стоить данная работа?");
 
 let adaptive = confirm("Нужен ли адаптив на сайте?");
 if(typeof adaptive != Boolean){
@@ -35,21 +44,33 @@ if(typeof adaptive != Boolean){
 }
 
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
-if(typeof service1 != String){
+if(service1 != null){
+    if(typeof service1 != String){
     service1 = service1.toString();
+    }
+    else{
+        service1 = "";
+    }
 }
+
 
 let servicePrice1 = prompt("Сколько это будет стоить?");
 
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
-if(typeof service2 != String){
+if(service2 != null){
+    if(typeof service2 != String){
     service2 = service2.toString();
+    }
+    else{
+        service2 = "";
+    }
 }
 
 let servicePrice2 = prompt("Сколько это будет стоить?");
 
 
 const getAllServicePrices = function(){
+    
     servicePrice1 = CheckPrice(servicePrice1, service1);
     servicePrice2 = CheckPrice(servicePrice2, service2);
 
@@ -58,11 +79,17 @@ const getAllServicePrices = function(){
 
 function CheckPrice(price, serv)
 {
-    let servP = parseInt(price.trim());
-    if ((isNaN(servP)||(servP<0)))
-    {
+    let servP;
+    if(price == null) {
+        servP = 0;
+    }
+    else{
+        servP = parseInt(price.trim());
+        if ((isNaN(servP)||(servP<0)))
+        {
         console.log("Введенное значение стоимости " + serv + " не является числом!");
         servP = 0;
+        }
     }
     return servP;
 }
