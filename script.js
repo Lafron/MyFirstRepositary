c3768359c77c07ebb5a7b17ba21bda2dc9c7ed70
Epshtein Dmitry <letuver@yahoo.com>
2021-12-01 19:52:05 +0200

    2 functions

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-let answ = parseInt(prompt("Enter a number: ").trim());
-let result = 0;
-if(!(isNaN(answ))){
-    result = answ;
+const arr = ["214","785","2095","474","1747","2112", "2907"];
+
+function SelectNum(array)
+{
+     for (let index = 0; index < array.length; index++) {
+         //let newArr = [];
+        
+         if (array[index].startsWith("2")) {
+             //newArr.push(array[index]);
+            console.log(array[index]);    
+         }
+         
+     }
 }
 
-console.log(result);
+SelectNum(arr);
+
+for(let i = 1; i < 100; i++)
+{
+    
+    if(checkNum(i)){
+        console.log(i + " Делители этого числа: " + i + " " + 1);
+    }
+}
+
+function checkNum(num)
+{
+    let result = false;
+    const arr = [2, 3, 5, 7];
+    
+    for (let i = 0; i < arr.length; i++) {
+             
+        if((num > arr[i]) && (num % arr[i] == 0))
+        {
+            result = false;
+            break;
+        }
+        else
+        {
+            result = true;
+        }
+    }
+    return result;
+}
