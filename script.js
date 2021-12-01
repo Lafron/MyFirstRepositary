70d7666ad25b18486ce4048eda34f1443aabc471
Epshtein Dmitry <letuver@yahoo.com>
2021-12-01 22:38:06 +0200

    null test

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let answ = parseInt(prompt("Enter a number: ").trim());
+let answ = prompt("Enter a number: ");
 let result = 0;
-if(!(isNaN(answ))){
-    result = answ;
+
+if(answ != null)
+{
+    answ = parseInt(answ.trim());
+    if(!(isNaN(answ))) {
+        result = answ;
+    }
 }
 
 console.log(result);
