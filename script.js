654397b0adee8b700d582518891935ed19781541
Epshtein Dmitry <letuver@yahoo.com>
2021-12-03 19:04:14 +0200

    add space

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ const Table = document.querySelector("#WeakTable");
 const d = new Date();
 let dayNow = d.getDay();
 
-for(let k in weak)
-{
+for(let k in weak) {
     k = parseInt(k);
     let classN = "";
-    if((k == 0)||(k == 6))
-    {
+
+    if((k == 0)||(k == 6)) {
         classN = 'class="hofesh"';
     }
-    if(k == dayNow)
-    {
+
+    if(k == dayNow) {
         classN = 'class="today"';
     }
+    
     Table.innerHTML += "<span "+ classN + ">" + weak[k] + "</span><br/>";
 } 
