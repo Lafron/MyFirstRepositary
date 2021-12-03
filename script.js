4ee29a0c837fb5d0400db72aeaf49aa927ea8605
Epshtein Dmitry <letuver@yahoo.com>
2021-12-03 18:40:14 +0200

    add classes

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
-const arr = ["214","785","2095","474","1747","2112", "2907"];
+const weak = ["воскресенье", "понедельник", "вторник", "среда", "четверг", 
+"пятница", "суббота"];
 
-function SelectNum(array)
+const Table = document.querySelector("#WeakTable");
+
+const d = new Date();
+let dayNow = d.getDay();
+
+console.log(dayNow);
+for(let k in weak)
 {
-     for (let index = 0; index < array.length; index++) {
-         //let newArr = [];
-        
-         if (array[index].startsWith("2")) {
-             //newArr.push(array[index]);
-            console.log(array[index]);    
-         }
-         
-     }
-}
-
-SelectNum(arr);
-
-for(let i = 1; i < 100; i++)
-{
-    
-    if(checkNum(i)){
-        console.log(i + " Делители этого числа: " + i + " " + 1);
+    let classN = "";
+    if((k==0)||(k==6))
+    {
+        classN = 'class="hofesh"';
     }
-}
-
-function checkNum(num)
-{
-    let result = false;
-    const arr = [2, 3, 5, 7];
-    
-    for (let i = 0; i < arr.length; i++) {
-             
-        if((num > arr[i]) && (num % arr[i] == 0))
-        {
-            result = false;
-            break;
-        }
-        else
-        {
-            result = true;
-        }
+    if(k==dayNow.toString())
+    {
+        classN = 'class="today"';
     }
-    return result;
-}
+    Table.innerHTML += "<span "+ classN + ">" + weak[k] + "</span><br/>";
+} 
