b3acb34c32e3e8b5749225b4aaaff90ffcc90722
Epshtein Dmitry <letuver@yahoo.com>
2021-12-08 02:45:11 +0200

    setInterval

--- script.js
+++ script.js
@@ -1,22 +1,76 @@
-const weak = ["воскресенье", "понедельник", "вторник", "среда", "четверг", 
-"пятница", "суббота"];
+const Table = document.querySelector("#TimeTable");
 
-const Table = document.querySelector("#WeakTable");
+function Start(){
+    let d = new Date();
 
-const d = new Date();
-let dayNow = d.getDay();
+    let myDate = {
+        weakDay: d.getDay(),
+        monDay: d.getDate(),
+        month: d.getMonth(),    
+        year: d.getFullYear(),
 
-for(let k in weak) {
-    k = parseInt(k);
-    let classN = "";
+        hour: d.getHours(),
+        min: d.getMinutes(),
+        second: d.getSeconds(),
+    };
 
-    if((k == 0)||(k == 6)) {
-        classN = 'class="hofesh"';
+    printDate(myDate);
+}
+
+function getHourIndex(myDate) {
+    const hourArr=["час", "часа", "часов"];
+    let hourIndex="";
+
+    if((myDate.hour==1)||myDate.hour==21){
+        hourIndex = hourArr[0];
+        }
+     else if (((myDate.hour>1)&&(myDate.hour<5))||(myDate.hour>21)) {
+        hourIndex = hourArr[1];
+        }
+    else{
+        hourIndex = hourArr[2];
+        }
+
+    return hourIndex;
+}
+
+
+function setZero(myDate){
+    for(let k in myDate){
+        if(parseInt(myDate[k]) < 10){
+            myDate[k] = "0" + (myDate[k]);
+            }
+        }
+        return myDate;
     }
 
-    if(k == dayNow) {
-        classN = 'class="today"';
-    }
+
+function printDate(myDate){
+    const weak = ["воскресенье", "понедельник", "вторник", "среда", "четверг", 
+    "пятница", "суббота"];
+
+    const months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", 
+    "сентября", "октября", "норября", "декабря"];
+
+    const longMonth = months[myDate.month];
+    const longWeakDay = weak[myDate.weakDay];
+
+    let hourIndex = getHourIndex(myDate);
     
-    Table.innerHTML += "<span "+ classN + ">" + weak[k] + "</span><br/>";
-} 
+    myDate = setZero(myDate);
+    
+    Table.innerHTML = "";
+    Table.innerHTML += "Сегодня " + longWeakDay + ", " + myDate.monDay + " " + 
+        longMonth + " " + myDate.year + " года, " + myDate.hour + " " + 
+        hourIndex + " " + myDate.min + " минут " + myDate.second + " секунд<br/>";
+
+    Table.innerHTML += myDate.monDay + "." + myDate.month + "." + myDate.year + 
+        " - " + myDate.hour + ":" + myDate.min + ":" + myDate.second + "<br/>";
+    }
+
+Start();
+setInterval(Start, 1000);
+
+
+
+
